6b6f1f72e80a5bee00888eb6036af629aa9e71e3
acampos91 <acampos91@hotmail.com>
2017-11-30 16:46:50 +0100

    iteration first and second

---
+++ starter-code/client/src/app/entry-list/entry-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { JournalService } from '../../services/journal.service';
+@Component({
+  selector: 'app-entry-list',
+  templateUrl: './entry-list.component.html',
+  styleUrls: ['./entry-list.component.css']
+})
+export class EntryListComponent implements OnInit {
+journal:Array<any> = [];
+  constructor(
+    public journalService: JournalService
+  ) {
+    this.journalService.getJournalList().subscribe( list =>{
+      this.journal = list;
+    });
+   }
+
+  ngOnInit() {
+  }
+
+}
